0768ba46c5dcf5c4c7c9d3cb59f64de249bd3694
Shift-Ronan <rleighton@shiftenvironmental.com>
2022-11-09 15:19:10 -0800

    Made relay way more efficient

--- mavlink_relay.py
+++ mavlink_relay.py
@@ -3,7 +3,7 @@ from time import sleep
 import socketio
 
 # Start listening to serial port
-com = mavutil.mavlink_connection('COM14')
+com = mavutil.mavlink_connection('udpin:localhost:1234')
 
 # TCP Relay Server
 SOCKET_URL="http://localhost:5000"
@@ -12,46 +12,44 @@ print(f"\nConnecting to Socket: {SOCKET_URL}")
 sio.connect(SOCKET_URL)
 print("Connected")
 
-
+# Message types to relay (mavlink.io/en/messages/)
 message_types = [
+    'ARDUINO_SENSE',
     'HEARTBEAT',
     'GLOBAL_POSITION_INT',
-    'ARDUINO_SENSE'
+    'GPS_STATUS',
+    'SYS_STATUS',
+    'RADIO_STATUS'
     ]
 
-# Parse message_types messages into dictionary
-def get_data(name):
-    msg = com.recv_match(type=name, blocking=False) # Look for new messages
-    if not msg:
-        return False
-    if msg.get_type() == "BAD_DATA":
-        if mavutil.all_printable(msg.data):
-            sys.stdout.write(msg.data)
-            sys.stdout.flush()
-        return False
-    
-    message_dict = {name: {}} # Create message dictionary
+def to_dict(msg):
+    for name in message_types:
+        if msg.get_type() == name:
+            message_dict = {name: {}} # Create message dictionary
 
+            for field in msg.fieldnames:
+                try:
+                    message_dict[name][field] = getattr(msg, field)
+                except:
+                    return False
+                    break
+            return message_dict
+    return False
 
-    for field in msg.fieldnames:
-        try:
-            message_dict[name][field] = getattr(msg, field)
-        except:
-            return False
-            break
-    return message_dict
-            
-           
 
 while True:
     try:
-        for name in message_types:
-            mavlink_data = get_data(name)
-            if not mavlink_data:
-                print(f'Error parsing message: {name}')
+        ''' Get message and error check '''
+        msg = com.recv_match();
+        if msg is not None:                
+            ''' Convert to dictionary '''
+            msg_dict = to_dict(msg)
+            if not msg_dict:
+                continue
+                #print(f'Ignoring: {msg.get_type()}')
             else:
-                print(f'Relaying msg: {name}')
-                sio.emit("mavlink_data", mavlink_data)
+                print(f'Relaying msg: {msg.get_type()}')
+                sio.emit("mavlink_data", msg_dict)
 
     except KeyboardInterrupt:
         print("Exiting...")
