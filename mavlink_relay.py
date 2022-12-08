62eb27b194bbcfcce4be548628691d53fd373e0b
Tyson Fritz <tfritz@shiftenvironmental.com>
2022-12-08 10:01:38 -0800

    Cleanup and parameterize connection string.

--- mavlink_relay.py
+++ mavlink_relay.py
@@ -2,10 +2,13 @@ from pymavlink import mavutil
 import socketio
 import time
 
+
+# MAVLINK_HOST = 'udpin:127.0.0.1:5760' # Serial Port / QGC Relay
+MAVLINK_HOST = 'tcp:127.0.0.1:5760' # SITL
 MESSAGE_FREQUENCY = 2 # Number of seconds before each message poll
 MAVLINK_CONNECTION_TIMEOUT = 2 # Try to reconnect after this many failed message polls
 
-message_types = [
+RELEVANT_MESSAGES = [
    'ARDUINO_SENSE',
    'HEARTBEAT',
    'GLOBAL_POSITION_INT',
@@ -15,8 +18,7 @@ message_types = [
 ]
 
 def connect_mavlink():
-   # mavlink = mavutil.mavlink_connection('tcp:127.0.0.1:5760') # SITL
-   mavlink = mavutil.mavlink_connection('udpin:127.0.0.1:5760') # Serial Port / QGC Relay
+   mavlink = mavutil.mavlink_connection(MAVLINK_HOST)
    print("Waiting for heartbeat...")
    mavlink.wait_heartbeat()
    print("Heartbeat received: (system %u component %u)" % (mavlink.target_system, mavlink.target_component))
@@ -33,14 +35,14 @@ def connect_sio():
 
 def to_dict(msg):
     print(msg.get_type())
-    for name in message_types:
-        if msg.get_type() == name: # Check if the message type is wanted
-            message_dict = {name: {}} # Create message dictionary
+    for message_type in RELEVANT_MESSAGES:
+        if msg.get_type() == message_type: # Check if the message type is relevant
+            message_dict = {message_type: {}} # Create message dictionary
             message_dict['identity'] = { 'system': mavlink.target_system, 'component': mavlink.target_component }
 
             for field in msg.fieldnames:
                 try:
-                    message_dict[name][field] = getattr(msg, field)
+                    message_dict[message_type][field] = getattr(msg, field)
                 except:
                      return False
             return message_dict
@@ -52,8 +54,8 @@ def listen(mavlink):
       try:
          # Get message and error check
          msg = mavlink.recv_match();
-         if msg is not None:
-            silence = 0
+         if msg is not None:   
+            silence = 0             
             # Convert message to dictionary and relay it
             msg_dict = to_dict(msg)
             if msg_dict:
@@ -70,6 +72,7 @@ def listen(mavlink):
 
       except Exception as e:
          print("Exception: ", e)
+         # Something went wrong. Try again soon
          time.sleep(MESSAGE_FREQUENCY)
 
 
